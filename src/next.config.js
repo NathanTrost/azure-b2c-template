22cbf935af279b7de09c3abdff52ca455f57fca1
Nathan Trost <n.d.trost@gmail.com>
2021-04-20 08:12:04 -0500

    Account for static links - HACK

--- src/next.config.js
+++ src/next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   distDir: "out",
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/"
+    : "",
 };
