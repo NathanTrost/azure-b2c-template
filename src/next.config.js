f5ae4b32a62c87d972dd47b8c116297cef3b8a67
Nathan Trost <n.d.trost@gmail.com>
2021-04-20 09:39:21 -0500

    Add prefix

--- src/next.config.js
+++ src/next.config.js
@@ -1,8 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
   // Use the CDN in production and localhost for development.
-  // assetPrefix: isProd
-  //   ? "https://cdn.statically.io/gh/NathanTrost/azure-b2c-template/gh-pages/"
-  //   : "",
+  assetPrefix: ".",
 };
