f272f11d72e3a22a7fb173db1b0079d5fe3bf57f
Nathan Trost <n.d.trost@gmail.com>
2021-04-20 09:03:24 -0500

    Merge all the things

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,36 @@
 import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Testing </title>
-      </Head>
-      <div className="homepage">
-        <main>
-          <div>This is just a test</div>
-        </main>
+    <div className={styles.container}>
+      {/* <Head>
+        <title>Sign up or sign in</title>
+
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head> */}
+
+      <div class="container unified_container" role="presentation">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <h1>The Puzzle Society</h1>
+
+                <div id="api"></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+
+      <div id="shadowMeasureIt"></div>
+      <div id="divCoordMeasureIt"></div>
+      <div id="divRectangleMeasureIt">
+        <div id="divRectangleBGMeasureIt"></div>
+      </div>
+    </div>
   );
 }
