89fd5ec67a7f441b12beba8e03c159e5366eae35
Nathan Trost <n.d.trost@gmail.com>
2021-04-20 08:45:12 -0500

    Quick test

--- src/next.config.js
+++ src/next.config.js
@@ -1,9 +1,8 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  distDir: "out",
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/"
+    ? "https://cdn.statically.io/gh/NathanTrost/azure-b2c-template/gh-pages/"
     : "",
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "../components/providers";
+import "../styles.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
