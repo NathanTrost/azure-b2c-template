42ed415ed5427410ec4f6e1aeb4167fa3b539063
Nathan Trost <n.d.trost@gmail.com>
2021-04-20 09:22:38 -0500

    Remove statically?

--- src/next.config.js
+++ src/next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/NathanTrost/azure-b2c-template/gh-pages/"
-    : "",
+  // assetPrefix: isProd
+  //   ? "https://cdn.statically.io/gh/NathanTrost/azure-b2c-template/gh-pages/"
+  //   : "",
 };
