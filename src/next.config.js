640adca45a6692c62ac7e05bf936956cd6ddda8c
Nathan Trost <n.d.trost@gmail.com>
2021-04-20 09:42:58 -0500

    Allow for static resources in gh-pages

--- src/next.config.js
+++ src/next.config.js
@@ -1,4 +1,5 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: ".",
+  assetPrefix: isProd ? "." : "",
 };
